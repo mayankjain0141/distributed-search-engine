be13a565526bca462ec2852ff83d46a5acafdabb
Apoorv77 <55060569+Apoorv77@users.noreply.github.com>
2022-11-14 02:52:02 +0530

    update sparkExecutor

--- src/main/java/Spark/SparkExecutor.java
+++ src/main/java/Spark/SparkExecutor.java
@@ -1,23 +1,62 @@
+//package Spark;
+//
+//public class SparkExecutor {
+//    public static String SparkDriver(String op){
+//        System.out.println(op);
+//        if (op == "invertedIndex"){
+//            new InvertedIndex().Run();
+//        }
+//        else if (op == "searchWord"){
+//            new SearchWord().Run("bigicontop");
+//        }
+//        else if (op == "searchPhrase"){
+//            new SearchPhrase().Run("AND", "modem device that allows computer");
+//        }
+//        else if(op == "positivityRank"){
+//            new PositivityRank().Run();
+//        }
+//        return op;
+//    }
+
 package Spark;
 
 public class SparkExecutor {
-    public static String SparkDriver(String op){
-        System.out.println(op);
-        if (op == "invertedIndex"){
-            new InvertedIndex().Run();
+        public static String buildIndex(){
+            try {
+                // call build index
+                new InvertedIndex().Run();
+            }catch(Exception e){
+                System.out.println("*** Exception in build index ***");
+            }
+            return "";
         }
-        else if (op == "searchWord"){
-            new SearchWord().Run("bigicontop");
+        public static String searchWord(String word){
+            try {
+                //call search word
+                new SearchWord().Run(word);
+            }catch(Exception e){
+                System.out.println("*** Exception in search word ***");
+            }
+            return "";
         }
-        else if (op == "searchPhrase"){
-            new SearchPhrase().Run("AND", "modem device that allows computer");
+        public static String searchPhrase(String op,String phrase){
+            try {
+                // call search phrase
+                new SearchPhrase().Run(op,phrase);
+            }catch(Exception e){
+                System.out.println("*** Exception in search phrase ***");
+            }
+            return "";
         }
-        else if(op == "positivityRank"){
-            new PositivityRank().Run();
+        public static String computePositivityRank(){
+            try {
+                //call ranking func
+                new PositivityRank().Run();
+            }catch(Exception e){
+                System.out.println("*** Exception in computing Positivity rank ***");
+            }
+            return "";
         }
-        return op;
+
     }
-
-
-    
 }
